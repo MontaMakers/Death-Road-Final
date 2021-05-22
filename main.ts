ea1e9df9570c504ffb8216c79da2ba06a82f5748
Mike-Monta <83614687+Mike-Monta@users.noreply.github.com>
2021-05-22 17:29:27 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,22 @@
 scene.setBackgroundColor(1)
 scene.setBackgroundImage(assets.image`Menu`)
-let car1 = sprites.create(assets.image`car2`, SpriteKind.Player)
-let mySprite2 = sprites.create(assets.image`car3`, SpriteKind.Player)
+let car1 = sprites.create(assets.image`car0`, SpriteKind.Player)
+let humo = sprites.create(assets.image`humo1`, SpriteKind.Player)
 tiles.setTilemap(tilemap`level1`)
 scene.cameraFollowSprite(car1)
+tiles.placeOnTile(car1, tiles.getTileLocation(7, 32))
 controller.moveSprite(car1)
 game.onUpdate(function () {
     if (controller.right.isPressed()) {
-        car1.setImage(assets.image`car2`)
+        car1.setImage(assets.image`car1`)
     }
     if (controller.left.isPressed()) {
-        car1.setImage(assets.image`car3`)
+        car1.setImage(assets.image`car6`)
     }
     if (controller.up.isPressed()) {
         car1.setImage(assets.image`car0`)
     }
     if (controller.down.isPressed()) {
-        car1.setImage(assets.image`car1`)
+        car1.setImage(assets.image`car3`)
     }
 })
