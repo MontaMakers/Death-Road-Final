3d637e41511efc72cec5333f540d06dcbb11fd73
Mike-Monta <83614687+Mike-Monta@users.noreply.github.com>
2021-05-22 21:18:33 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,15 @@ scene.setBackgroundColor(1)
 scene.setBackgroundImage(assets.image`Menu`)
 let car1 = sprites.create(assets.image`car0`, SpriteKind.Player)
 let humo = sprites.create(assets.image`humo1`, SpriteKind.Player)
+humo.setImage(assets.image`vacio`)
 tiles.setTilemap(tilemap`level1`)
 scene.cameraFollowSprite(car1)
 tiles.placeOnTile(car1, tiles.getTileLocation(7, 32))
-controller.moveSprite(car1)
+tiles.placeOnTile(humo, tiles.getTileLocation(7, 32))
+controller.moveSprite(car1, 100, 100)
+game.onUpdate(function () {
+	
+})
 game.onUpdate(function () {
     if (controller.right.isPressed()) {
         car1.setImage(assets.image`car1`)
@@ -18,5 +23,17 @@ game.onUpdate(function () {
     }
     if (controller.down.isPressed()) {
         car1.setImage(assets.image`car3`)
+    }
+    if (controller.right.isPressed() || (controller.up.isPressed() || (controller.down.isPressed() || controller.left.isPressed()))) {
+        humo.setImage(assets.image`humo1`)
+        humo.follow(car1, 98)
+        humo.startEffect(effects.ashes, 100)
+    } else {
+        humo.setImage(assets.image`vacio`)
+    }
+})
+game.onUpdate(function () {
+    if (false && controller.up.isPressed()) {
+    	
     }
 })
